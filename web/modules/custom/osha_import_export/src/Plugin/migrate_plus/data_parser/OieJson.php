6962995ff1753227e28b09b07ed429c649bd6632
rramos78 <ruben.ramos@bilbomatica.es>
2022-02-11 14:35:12 +0100

    Imports fix

--- web/modules/custom/osha_import_export/src/Plugin/migrate_plus/data_parser/OieJson.php
+++ web/modules/custom/osha_import_export/src/Plugin/migrate_plus/data_parser/OieJson.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\osha_import_export\Plugin\migrate_plus\data_parser;
 
+use Drupal\Core\Database\Connection;
 use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json as MigrateJson;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Obtain JSON data for migration.
@@ -59,8 +61,9 @@ class OieJson extends MigrateJson implements ContainerFactoryPluginInterface {
     $items_new = [];
     foreach ($items as $i => $item) {
       $nm_item = $item['item'];
-      $nm_nid = $nm_item['nid'];
-      $nm_url = $nm_root_url . '/export/node/' . $nm_nid;
+      //$nm_nid = $nm_item['nid'];
+      $nm_nid = '';
+      $nm_url = $nm_root_url . '/export/node/' . $nm_item['nid'];
       $response = $this->getDataFetcherPlugin()->getResponseContent($nm_url);
       $response_data = json_decode($response, TRUE);
 
@@ -72,18 +75,27 @@ class OieJson extends MigrateJson implements ContainerFactoryPluginInterface {
       $decoded_data = json_decode($response, TRUE);
 
       // If the node already exists, update it.
-      $node = $this->entityTypeManager->getStorage('node')->loadByProperties(['title' => $decoded_data['title']]);
+      $node = $this->entityTypeManager->getStorage('node')->loadByProperties(['title' => $decoded_data['title'],'type' => $this->configuration['content_type']]);
       if (!empty($node)) {
-        /** @var \Drupal\node\Entity\Node  $node */
+        /** @var \Drupal\node\Entity\Node $node */
         $node = reset($node);
         $decoded_data['nid'] = $node->id();
+      }else{
+        //Get next not use the old email avoiding data lost
+        $results = \Drupal::database()->query('select max(nid) nextid from node')->fetchAll();
+        $decoded_data['nid'] = $results[0]->nextid + 1;
+      }
+      //The content from Hwc whit the show in ncw unchecked will be migrated in draft status
+      if ($decoded_data['field_show_in_ncw']["und"][0]["value"]==0 && $url=='hwc'){
+        $decoded_data["status"] = 0;
+        $decoded_data["workbench_moderation"]["current"]["state"] = 'draft';
+        unset($decoded_data["workbench_moderation"]["published"]);
       }
 
       // Set the new item.
       $items_new[$i]['item'] = $decoded_data;
     }
 
-    //ksm($items_new);
     return $items_new;
   }
 
