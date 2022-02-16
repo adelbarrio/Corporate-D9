6573bf1141d031fa7aa8feff99f70fd9fe85b098
wodby <wodby@example.com>
2022-02-16 09:29:48 +0000

    HWC import ceon

--- web/modules/custom/hwc_import_export/src/HwcCron.php
+++ web/modules/custom/hwc_import_export/src/HwcCron.php
@@ -13,8 +13,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 /**
  * General class for Cron hooks.
  */
-class HwcCron implements ContainerInjectionInterface
-{
+class HwcCron implements ContainerInjectionInterface {
 
   use StringTranslationTrait;
 
@@ -23,15 +22,18 @@ class HwcCron implements ContainerInjectionInterface
    *
    * @var \Drupal\migrate\Plugin\MigrationPluginManager
    */
-  protected $migrationManager;
+  protected MigrationPluginManager $migrationManager;
 
   /**
    * The entity type manager.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $entityTypeManager;
+  protected EntityTypeManagerInterface $entityTypeManager;
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct(MigrationPluginManager $migration_manager, EntityTypeManagerInterface $entity_type_manager) {
     $this->migrationManager = $migration_manager;
     $this->entityTypeManager = $entity_type_manager;
@@ -53,15 +55,65 @@ class HwcCron implements ContainerInjectionInterface
    */
   public function cron() {
     $migrations = [
-      'import_crm_board',
-      'import_crm_bureauforweb',
-      'import_crm_focalpoints',
-      'import_crm_advisorygroups_prag',
-      'import_crm_advisorygroups_agcp',
-      'import_crm_advisorygroups_wesag',
+      'hwc_publications',
+      'hwc_publications_bg',
+      'hwc_publications_cs',
+      'hwc_publications_da',
+      'hwc_publications_de',
+      'hwc_publications_es',
+      'hwc_publications_de',
+      'hwc_publications_el',
+      'hwc_publications_et',
+      'hwc_publications_fi',
+      'hwc_publications_hr',
+      'hwc_publications_hu',
+      'hwc_publications_is',
+      'hwc_publications_it',
+      'hwc_publications_lv',
+      'hwc_publications_lt',
+      'hwc_publications_nl',
+      'hwc_publications_mt',
+      'hwc_publications_no',
+      'hwc_publications_pl',
+      'hwc_publications_pt',
+      'hwc_publications_ro',
+      'hwc_publications_sk',
+      'hwc_publications_sl',
+      'hwc_publications_sv',
+      'hwc_publications_fr',
+      'hwc_add_publications',
       'hwc_news',
+      'hwc_add_news',
+      'hwc_highlights',
+      'hwc_highlights_bg',
+      'hwc_highlights_cs',
+      'hwc_highlights_da',
+      'hwc_highlights_de',
+      'hwc_highlights_es',
+      'hwc_highlights_de',
+      'hwc_highlights_el',
+      'hwc_highlights_et',
+      'hwc_highlights_fi',
+      'hwc_highlights_hr',
+      'hwc_highlights_hu',
+      'hwc_highlights_is',
+      'hwc_highlights_it',
+      'hwc_highlights_lv',
+      'hwc_highlights_lt',
+      'hwc_highlights_nl',
+      'hwc_highlights_mt',
+      'hwc_highlights_no',
+      'hwc_highlights_pl',
+      'hwc_highlights_pt',
+      'hwc_highlights_ro',
+      'hwc_highlights_sk',
+      'hwc_highlights_sl',
+      'hwc_highlights_sv',
+      'hwc_highlights_fr',
+      'hwc_add_highlights',
+      'hwc_infographic',
+      'hwc_add_infographic',
       'hwc_events',
-      'oira_news',
     ];
 
     // Start every migration.
